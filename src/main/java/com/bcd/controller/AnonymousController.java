28049830cccf2c256ddde73961740ad1203c1fb7
baichangda <471267877@qq.com>
2019-01-19 20:13:17 +0800

    c

--- src/main/java/com/bcd/controller/AnonymousController.java
+++ src/main/java/com/bcd/controller/AnonymousController.java
@@ -6,7 +6,6 @@ import com.bcd.base.util.I18nUtil;
 import com.bcd.service.ApiService;
 import com.bcd.sys.keys.KeysConst;
 import io.swagger.annotations.*;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.session.Session;
@@ -14,10 +13,8 @@ import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -36,8 +33,7 @@ public class AnonymousController extends BaseController{
     @Autowired
     ApiService apiService;
 
-    @Value("${wx.token}")
-    String wxToken;
+
 
     @SuppressWarnings("unchecked")
     @RequestMapping(value = "/getPublicKey",method = RequestMethod.GET)
@@ -67,29 +63,4 @@ public class AnonymousController extends BaseController{
         response(workbook,toDateFileName(fileName),response);
         return JsonMessage.success();
     }
-
-    @SuppressWarnings("unchecked")
-    @RequestMapping(value = "/wxToken",method = RequestMethod.GET)
-    @ApiOperation(value = "微信Token验证",notes = "微信Token验证")
-    @ApiResponse(code = 200,message = "Token",response = String.class)
-    public String wxToken(
-            @RequestParam(required = false) String signature,
-            @RequestParam(required = false) String timestamp,
-            @RequestParam(required = false) String nonce,
-            @RequestParam(required = false) String echostr){
-        logger.info("\nsignature: "+signature+"\ntimestamp: "+timestamp+"\nnonce: "+nonce+"\nechostr: "+echostr+"\n");
-        List<String> list=new ArrayList<>();
-        list.add(nonce);
-        list.add(timestamp);
-        list.add(wxToken);
-        Collections.sort(list);
-        String res=DigestUtils.sha1Hex(list.stream().reduce((e1,e2)->e1+e2).orElse(""));
-        logger.info("\nres: "+res);
-        if(res.equals(signature)){
-            return echostr;
-        }else{
-            return null;
-        }
-    }
-
 }
