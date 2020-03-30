c4d13986e7c175040c5e7a2a0e3bb2d56ddefd87
baichangda <471267877@qq.com>
2020-03-30 15:23:44 +0800

    c

--- Wx/src/main/java/com/bcd/wx/handler/TestHandler.java
+++ Wx/src/main/java/com/bcd/wx/handler/TestHandler.java
@@ -1,10 +1,10 @@
 package com.bcd.wx.handler;
 
 import com.bcd.base.exception.BaseRuntimeException;
-import com.bcd.base.util.FileUtil;
 import com.bcd.base.util.SpringUtil;
 import com.bcd.wx.bean.WxUserBean;
 import com.bcd.wx.service.WxUserService;
+import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpMessageHandler;
@@ -19,6 +19,9 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
 @Component
@@ -32,9 +35,10 @@ public class TestHandler implements WxMpMessageHandler {
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> context, WxMpService wxMpService, WxSessionManager sessionManager) throws WxErrorException {
         logger.info("receive[{}]",wxMessage.toString());
         WxUserBean wxUserBean= wxUserService.findByOpenId(wxMessage.getFromUser());
-        try {
-            File imageFile = SpringUtil.getResource("image/test1.jpg").getFile();
-            WxMediaImgUploadResult wxMediaImgUploadResult= wxMpService.getMaterialService().mediaImgUpload(imageFile);
+        try (InputStream is = SpringUtil.getResource("image/test1.jpg").getInputStream()){
+            Path path=Files.createTempFile("test1","jpg");
+            Files.copy(is,path);
+            WxMediaImgUploadResult wxMediaImgUploadResult= wxMpService.getMaterialService().mediaImgUpload(path.toFile());
             String imageUrl=wxMediaImgUploadResult.getUrl();
             return WxMpXmlOutMessage.IMAGE().fromUser(wxMessage.getToUser()).toUser(wxMessage.getFromUser())
                     .mediaId(imageUrl).build();
