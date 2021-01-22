a9b65b5102ed4aec714a9bd707b2b3ba716c66d0
baichangda <471267877@qq.com>
2021-01-22 16:52:41 +0800

    c

--- MongoDB/src/main/java/com/bcd/mongodb/condition/converter/StringConditionConverter.java
+++ MongoDB/src/main/java/com/bcd/mongodb/condition/converter/StringConditionConverter.java
@@ -3,13 +3,12 @@ package com.bcd.mongodb.condition.converter;
 import com.bcd.base.condition.Converter;
 import com.bcd.base.condition.impl.StringCondition;
 import com.bcd.base.exception.BaseRuntimeException;
-import org.assertj.core.util.Arrays;
 import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.util.StringUtils;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -23,54 +22,79 @@ public class StringConditionConverter implements Converter<StringCondition,Crite
         Object val=condition.val;
         StringCondition.Handler handler=condition.handler;
         Criteria criteria= null;
-        if(!StringUtils.isEmpty(val)){
-            criteria=Criteria.where(fieldName);
-            switch (handler){
-                case EQUAL: {
-                    criteria.is(val);
-                    break;
-                }
-                case NOT_EQUAL: {
-                    criteria.ne(val);
-                    break;
-                }
-                case ALL_LIKE: {
-                    criteria.regex(".*"+val.toString()+".*");
-                    break;
-                }
-                case LEFT_LIKE: {
-                    criteria.regex("^("+val.toString()+").*");
-                    break;
-                }
-                case RIGHT_LIKE: {
-                    criteria.regex(".*("+val.toString()+")$");
-                    break;
-                }
-                case IN: {
-                    if(val instanceof Collection){
-                        List notEmptyList= (List)((Collection) val).stream().filter(Objects::nonNull).collect(Collectors.toList());
+        if(val!=null){
+            if(val instanceof Collection){
+                criteria=Criteria.where(fieldName);
+                List notEmptyList= (List)((Collection) val).stream().filter(e->e!=null&&!e.toString().isEmpty()).collect(Collectors.toList());
+                switch (handler){
+                    case IN:{
                         criteria.in(notEmptyList);
-                    }else if(Arrays.isArray(val)){
-                        List notEmptyList=Arrays.asList(val).stream().filter(e->e!=null).collect(Collectors.toList());
-                        criteria.in(notEmptyList);
-                    }else{
-                        throw BaseRuntimeException.getException("[StringConditionConverter.convert],Value Must be Collection Instance!");
+                        break;
                     }
-                    break;
-                }
-                case NOT_IN: {
-                    if(val instanceof Collection){
-                        List notEmptyList= (List)((Collection) val).stream().filter(Objects::nonNull).collect(Collectors.toList());
+                    case NOT_IN:{
                         criteria.nin(notEmptyList);
-                    }else{
+                        break;
+                    }
+                    default:{
                         throw BaseRuntimeException.getException("[StringConditionConverter.convert],Value Must be Collection Instance!");
                     }
-                    break;
                 }
-                default :{
-                    throw BaseRuntimeException.getException("[StringConditionConverter.convert],Do Not Support ["+handler+"]!");
+            }else if(val.getClass().isArray()){
+                criteria=Criteria.where(fieldName);
+                List notEmptyList=new ArrayList();
+                int len= Array.getLength(val);
+                if(len!=0){
+                    for(int i=0;i<=len-1;i++){
+                        Object o=Array.get(val,i);
+                        if(o!=null&&!o.toString().isEmpty()){
+                            notEmptyList.add(o);
+                        }
+                    }
+                }
+                switch (handler){
+                    case IN:{
+                        criteria.in(notEmptyList);
+                        break;
+                    }
+                    case NOT_IN:{
+                        criteria.nin(notEmptyList);
+                        break;
+                    }
+                    default:{
+                        throw BaseRuntimeException.getException("[StringConditionConverter.convert],Value Must be Array Instance!");
+                    }
+                }
+            }else{
+                if(!val.toString().isEmpty()) {
+                    criteria=Criteria.where(fieldName);
+                    switch (handler) {
+                        case EQUAL: {
+                            criteria.is(val);
+                            break;
+                        }
+                        case NOT_EQUAL: {
+                            criteria.ne(val);
+                            break;
+                        }
+                        case ALL_LIKE: {
+                            criteria.regex(".*(" + val.toString() + ").*");
+                            break;
+                        }
+                        case LEFT_LIKE: {
+                            criteria.regex("^(" + val.toString() + ")");
+                            break;
+                        }
+                        case RIGHT_LIKE: {
+                            criteria.regex("(" + val.toString() + ")$");
+                            break;
+                        }
+                        default: {
+                            throw BaseRuntimeException.getException("[StringConditionConverter.convert],Do Not Support [" + handler + "]!");
+                        }
+                    }
                 }
             }
+
         }
         return criteria;
     }
